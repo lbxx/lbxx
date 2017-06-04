452e770a6dbbd8b497ccbbb02249a96952ab7481
tangxinmao <1051477653@qq.com>
2017-06-04 12:37:23 +0800

    集成shiro545

--- src/main/java/cn/lingco/common/emun/Code.java
+++ src/main/java/cn/lingco/common/emun/Code.java
@@ -4,5 +4,14 @@ package cn.lingco.common.emun;
  * Created by tangxinmao on 2017/5/30.
  */
 public enum Code {
+    CAPTCHA_ERROR//验证码错误
+    ;
+    public static void main(String[] args) {
 
+    }
 }
+
+
+
+
+
