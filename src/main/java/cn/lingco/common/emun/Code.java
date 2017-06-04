bf1c8b76989cc008195f8ceebd5753c15ccb250a
tangxinmao <1051477653@qq.com>
2017-06-04 11:03:09 +0800

    集成shiro545

---
+++ src/main/java/cn/lingco/common/emun/Code.java
@@ -0,0 +1,8 @@
+package cn.lingco.common.emun;
+
+/**
+ * Created by tangxinmao on 2017/5/30.
+ */
+public enum Code {
+
+}
---
+++ src/main/java/cn/lingco/common/vo/Captcha.java
@@ -0,0 +1,28 @@
+package cn.lingco.common.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by tangxinmao on 2017/6/2.
+ */
+public class Captcha  implements Serializable{
+    private String captcha;//验证码
+    private Date createTime;// 创建时间
+
+    public String getCaptcha() {
+        return captcha;
+    }
+
+    public void setCaptcha(String captcha) {
+        this.captcha = captcha;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
