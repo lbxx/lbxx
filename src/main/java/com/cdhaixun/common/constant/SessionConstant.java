a041f7eb6cdf7e326a9f22b6248691a01ba5b47c
tangxinmao <1051477653@qq.com>
2017-06-07 10:24:27 +0800

    jjchjghjghj

--- src/main/java/com/cdhaixun/common/constant/SessionConstant.java
+++ src/main/java/com/cdhaixun/common/constant/SessionConstant.java
@@ -4,6 +4,6 @@ package com.cdhaixun.common.constant;
  * Created by tangxinmao on 2017/6/4.
  */
 public interface SessionConstant {
-    static final String KAPTCHA_SESSION_KEY ="KAPTCHA_SESSION_KEY";
-    static final String KAPTCHA_SESSION_DATE ="KAPTCHA_SESSION_DATE";
+    String KAPTCHA_SESSION_KEY ="KAPTCHA_SESSION_KEY";
+    String KAPTCHA_SESSION_DATE ="KAPTCHA_SESSION_DATE";
 }
