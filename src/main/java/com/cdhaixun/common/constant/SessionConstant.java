b67c851a73b3b6dccc4b14b345f50d2d49ae1271
tangxinmao <1051477653@qq.com>
2017-06-10 21:05:41 +0800

    增加常量

--- src/main/java/com/cdhaixun/common/constant/SessionConstant.java
+++ src/main/java/com/cdhaixun/common/constant/SessionConstant.java
@@ -6,4 +6,6 @@ package com.cdhaixun.common.constant;
 public interface SessionConstant {
     String KAPTCHA_SESSION_KEY ="KAPTCHA_SESSION_KEY";
     String KAPTCHA_SESSION_DATE ="KAPTCHA_SESSION_DATE";
+    String MANAGER="manager";
+    String MENU_LIST="menuList";
 }
