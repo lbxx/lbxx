378db1b5668fdf4b3d931db7fcacbcda37ecbc40
lingco <632135186@qq.com>
2017-06-10 21:32:50 +0800

    Merge branch 'master' of https://github.com/lbxx/lbxx
    # Conflicts:
    #	src/main/java/com/cdhaixun/common/emun/Code.java
    #	src/main/java/com/cdhaixun/persistence/ManagerMapper.java

--- src/test/java/com/cdhaixun/TestUser.java
+++ src/test/java/com/cdhaixun/TestUser.java
@@ -1,18 +1,8 @@
 package com.cdhaixun;
 
-import java.util.List;
-
-import javax.annotation.Resource;
-
-import org.apache.log4j.Logger;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.alibaba.fastjson.JSON;
-
-import com.cdhaixun.domain.UserInfo;
 
 /**
 * 作者 lingco
@@ -21,66 +11,5 @@ import com.cdhaixun.domain.UserInfo;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:spring.xml","classpath:spring-mybatis.xml"})
 public class TestUser {
-	private static final Logger logger = Logger.getLogger(TestUser.class);
-	
-	@Resource(name="userService")
-    IUserService userService;
-	
-	/**
-	 * 查询全部数据
-	 */
-	@Test
-	public void findAll(){
-		List<UserInfo> userList = userService.selectAll();
-		logger.info(JSON.toJSON(userList));
-	}
-	/**
-	 * 通过id查询数据
-	 */
-	@Test
-	public void findUserById(){
-		UserInfo userInfo = userService.findById(1);
-		logger.info(JSON.toJSON(userInfo));
-	}
-	/**
-	 * 添加数据
-	 */
-	@Test
-	public void insertUser(){
-		UserInfo userInfo = new UserInfo();
-		userInfo.setUname("小光");
-		userInfo.setUnumber(19);
-		try{
-			userService.save(userInfo);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	/**
-	 * 删除数据
-	 */
-	@Test
-	public void deleteUser(){
-		UserInfo userInfo = new UserInfo();
-		userInfo.setId(1);
-		try{
-			userService.delete(userInfo);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	/**
-	 * 修改数据
-	 */
-	@Test
-	public void updateUser(){
-		UserInfo userInfo = new UserInfo();
-		userInfo.setUname("lignco");
-		userInfo.setId(2);
-		try {
-			userService.update(userInfo);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+
 }
