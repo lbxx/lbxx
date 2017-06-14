d0d2f2020e900a72fb9018f5f1de4146ae80fc79
tangxinmao <1051477653@qq.com>
2017-06-14 17:47:54 +0800

    jjchjghjghj

--- src/main/java/com/cdhaixun/common/appVo/Mobile.java
+++ src/main/java/com/cdhaixun/common/appVo/Mobile.java
@@ -4,10 +4,19 @@ import java.io.Serializable;
 
 /**
  * Created by tangxinmao on 2017/6/11.
- * 用于发送验证码
+ * 用于发送验证码和验证码登录
  */
 public class Mobile implements Serializable {
-   private String mobile;
+    private String mobile;
+    private String captcha;//验证码
+
+    public String getCaptcha() {
+        return captcha;
+    }
+
+    public void setCaptcha(String captcha) {
+        this.captcha = captcha;
+    }
 
     public String getMobile() {
         return mobile;
