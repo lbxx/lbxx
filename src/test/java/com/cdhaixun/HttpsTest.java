07bcac2c87681627138ff36459b30e0d22a81a07
tangxinmao <1051477653@qq.com>
2017-06-16 18:28:42 +0800

    jjchjghjghj

---
+++ src/test/java/com/cdhaixun/HttpsTest.java
@@ -0,0 +1,13 @@
+package com.cdhaixun;
+
+
+import ytx.org.apache.http.client.HttpClient;
+
+/**
+ * Created by tangxinmao on 2017/6/16.
+ */
+public class HttpsTest {
+    public static void main(String[] args) {
+
+    }
+}
