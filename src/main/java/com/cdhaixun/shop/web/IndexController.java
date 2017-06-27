c7667a7b7a572297279b8df81a75f10a537dd730
tangxinmao <1051477653@qq.com>
2017-06-27 19:30:03 +0800

    解决冲突

--- src/main/java/com/cdhaixun/shop/web/IndexController.java
+++ src/main/java/com/cdhaixun/shop/web/IndexController.java
@@ -1,38 +1,17 @@
 package com.cdhaixun.shop.web;
 
 
-import java.io.IOException;
-<<<<<<< HEAD
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-=======
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.cdhaixun.common.constant.SessionConstant;
-import com.cdhaixun.domain.Manager;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.cdhaixun.domain.Menu;
->>>>>>> branch 'master' of https://github.com/lbxx/lbxx.git
 import com.cdhaixun.shop.service.IMenuService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * 首页数据
