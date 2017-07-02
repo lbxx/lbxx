b75d82f19de77f804ff4e4115c12870152692093
tangxinmao <1051477653@qq.com>
2017-07-02 17:03:41 +0800

    增加预约相关接口

--- src/main/java/com/cdhaixun/shop/service/impl/AppointmentServiceImpl.java
+++ src/main/java/com/cdhaixun/shop/service/impl/AppointmentServiceImpl.java
@@ -1,10 +1,14 @@
 package com.cdhaixun.shop.service.impl;
 
 import com.cdhaixun.domain.Appointment;
+import com.cdhaixun.domain.AppointmentDetail;
 import com.cdhaixun.persistence.AppointmentMapper;
 import com.cdhaixun.shop.service.IAppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by tangxinmao on 2017/7/2.
@@ -20,5 +24,21 @@ public class AppointmentServiceImpl implements IAppointmentService {
         }else {
             appointmentMapper.updateByPrimaryKeySelective(appointment);
         }
+    }
+
+    @Override
+    public List<Appointment> findByStartTimeAndTechnicianId(Date starttime, Date endtime, Integer technicianid) {
+        Appointment appointment=new Appointment();
+        appointment.setTechnicianid(technicianid);
+        appointment.setStarttime(starttime);
+        appointment.setEndtime(endtime);
+        return appointmentMapper.selectByAppointment(appointment);
+    }
+
+    @Override
+    public List<Appointment> findByUserId(Integer userid) {
+        Appointment appointment=new Appointment();
+        appointment.setTechnicianid(userid);
+        return appointmentMapper.selectByAppointment(appointment);
     }
 }
